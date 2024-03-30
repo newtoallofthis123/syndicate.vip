97d15bf85485334d9b03d8d7efc06b4336457be0
Sainath Guddeti <130617791+Sainath944@users.noreply.github.com>
2024-03-30 23:27:38 +0530

    Add files via upload

---
+++ src/Render/tablestructure.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Syndicate\Render;
+require_once __DIR__ . '/../../support/lib/vendor/autoload.php';
+
+use Approach\Render\Attribute;
+use Approach\Render\Container;
+use Approach\Render\HTML;
+use Approach\Render\Node;
+use Approach\Render\Stream;
+use Stringable;
+
+// use Approach\Render\HTML;
+
+class TableStructure extends HTML
+{
+    public function __construct(
+        public Stringable|string|null $tag = 'table',
+        public null|string|Stringable $id = null,
+        null|string|array|Node|Attribute $classes = null,
+        public null|array|Attribute $attributes = null,
+        public null|string|Stringable|Stream|self $content = null,
+        public array $styles = [],
+        public bool $prerender = false,
+        public bool $selfContained = false,
+    ) {
+        parent::__construct(
+            $tag,
+            $id,
+            $classes,
+            $attributes,
+            $content,
+            $styles,
+            $prerender,
+            $selfContained ,
+        );
+    }
+}
