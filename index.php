a55f4c832382a0bb3777bcd07c5549c37ecaf994
mrohith29 <mariyalarohith29@gmail.com>
2024-03-30 18:05:48 +0530

    add button.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ namespace Syndicate;
 
 require_once __DIR__ . '/support/lib/vendor/autoload.php';
 
+use Syndicate\Render\Button;
 use Syndicate\Render\Form;
 use Syndicate\Render\Input;
 use Syndicate\Render\TextArea;
@@ -10,10 +11,13 @@ use Syndicate\Render\TextArea;
 $input = new Input(value: '12@wow.com', type: 'email', name: 'email');
 $form = new Form();
 
-$textarea = new TextArea(placeholder: "wow", rows: 10, cols: 5);
+$textarea = new TextArea(placeholder: "wow", rows: 1, cols: 5);
 // $textarea->content = "This is so cool";
 
 $form->addFormEle($input);
 $form->addFormEle($textarea);
 
+$button = new Button(placeholder: "Click", link:"https://github.com/mrohith29", width:"30", height:"20");
+
+$form->addFormEle($button);
 echo $form;
