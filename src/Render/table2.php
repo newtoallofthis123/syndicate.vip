fc92c8f0c0f1b55c47d0bc61f10c9880cdac6b32
Sainath Guddeti <130617791+Sainath944@users.noreply.github.com>
2024-04-01 21:06:38 +0530

    Updated table2.php
    updated table 2

--- src/Render/table2.php
+++ src/Render/table2.php
@@ -3,15 +3,15 @@ namespace Syndicate\Render;
 
 use Approach\Render\Attribute;
 use Approach\Render\Container;
-use Approach\Render\HTML;
+// use Approach\Render\HTML;
 use Approach\Render\Node;
 use Approach\Render\Stream;
 use Stringable;
 
-class Table2 extends HTML
+class Table2 extends Container
 {
   public function __construct(
-    public null|string|Stringable $tag = 'table',
+    public string $tag = 'table',
     public null|string|Stringable $id = null,
     null|string|array|Node|Attribute $classes = null,
     public null|array|Attribute $attributes = new Attribute,
@@ -23,27 +23,29 @@ class Table2 extends HTML
     public null|array $headers = [],
     public null|array $rows = [],
   ) {
-    parent::__construct(
-      tag: $tag,
-      id: $id,
-      classes: $classes,
-      attributes: $attributes,
-      content: $content,
-      styles: $styles,
-      prerender: $prerender,
-      selfContained: $selfContained,
-    );
+    parent::__construct([
+      'tag'=> [$tag],
+      'id'=> $id,
+      'classes'=> $classes,
+      'attributes'=> $attributes,
+      'content'=> $content,
+      'styles'=> $styles,
+      'prerender'=> $prerender,
+      'selfContained' => $selfContained,
+    ]);
+    $this->tag = $tag;
 
     $this->nodes[] = $this->Headers();
     // $this->nodes = array_merge($this->nodes, $this->Rows());
     $this->nodes = $this->nodes + $this->Rows();
   }
-
-  private function Headers(): HTML
+   
+  private function Headers(): Container
   {
-    $returningheaderRow = new HTML(tag: 'tr');
+    $returningheaderRow = new Container(); 
+    $returningheaderRow->tag = 'tr';
     foreach ($this->headers as $header) {
-      $returningheaderRow->nodes[] = new HTML(tag: 'th', content: $header);
+      $returningheaderRow->nodes[] = new Node('th', $header);
     }
     return $returningheaderRow;
   }
@@ -52,12 +54,15 @@ class Table2 extends HTML
   {
     $returnedrows = [];
     foreach ($this->rows as $row) {
-      $tableRow = new HTML(tag: 'tr');
+      $tableRow = new Container(); 
+      $tableRow->tag = 'tr';
       foreach ($row as $data) {
-        $tableRow->nodes[] = new HTML(tag: 'td', content: $data);
+        $tableRow->nodes[] = new Node('td', $data);
       }
       $returnedrows[] = $tableRow;
     }
     return $returnedrows;
   }
 }
+
+
