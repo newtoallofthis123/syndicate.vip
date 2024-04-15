ed0f2e54ba5d9b87d7c17bf33fc69cbaa23092f4
newtoallofthis123 <noobscience123@gmail.com>
2024-04-15 23:31:57 +0530

    Add TileCest Co-authored-by: Sainath Guddeti <Sainath944@users.noreply.github.com>

--- src/Render/Badge.php
+++ src/Render/Badge.php
@@ -1,20 +1,18 @@
 <?php
 namespace Syndicate\Render;
 
-use Approach\Imprint\test\test\display;
 use Approach\Render\Attribute;
 use Approach\Render\HTML;
 use Approach\Render\Node;
 use Stringable;
 use Traversable;
 
-use function PHPUnit\Framework\countOf;
-
 require_once __DIR__ . '/../../support/lib/vendor/autoload.php';
 
 class Badge extends HTML
 {
-    public function RenderHead(): Traversable{
+    public function RenderHead(): Traversable
+    {
         foreach (parent::RenderHead() as $value) {
             yield $value;
         }
@@ -26,17 +24,16 @@ class Badge extends HTML
         null|string|array|Node|Attribute $classes = null,
         public null|array|Attribute $attributes = new Attribute,
         public $content = null,
-        public array $styles = ["display"=> "flex", "text-decoration"=> "none"],
+        public array $styles = ['display' => 'flex', 'text-decoration' => 'none'],
         public bool $prerender = false,
         public bool $selfContained = false,
-
-        public null|Node|Stringable|string|self $name = "",
-        public null|Node|Stringable|string|self $value = "",
-        public null|Node|Stringable|string|self $link = "",
+        public null|Node|Stringable|string|self $name = '',
+        public null|Node|Stringable|string|self $value = '',
+        public null|Node|Stringable|string|self $link = '',
     ) {
         parent::__construct(
             tag: $tag,
-            id: $id,    
+            id: $id,
             classes: ['Badge'],
             attributes: $attributes,
             content: $content,
@@ -46,9 +43,9 @@ class Badge extends HTML
         );
 
         $name_div = new HTML(tag: 'span');
-        $name_div['link'] = new HTML(tag: 'a', attributes: ["href" => $link], content: $name);
+        $name_div['link'] = new HTML(tag: 'a', attributes: ['href' => $link], content: $name);
         $value_div = new HTML(tag: 'span');
-        $value_div['link'] = new HTML(tag: 'a', attributes: ["href" => $link], content: $value);
+        $value_div['link'] = new HTML(tag: 'a', attributes: ['href' => $link], content: $value);
         $this->nodes['name'] = $name_div;
         $this->name = &$this->nodes['name'];
         $this->nodes['value'] = $value_div;
@@ -57,8 +54,9 @@ class Badge extends HTML
 }
 
 // Styles have to look like
-/* 
-.badge { display: inline-block; //other container styles }
-.badge span:first-child{ // key styles }
-.badge span:last-child{ // value styles }
-*/
+
+/*
+ * .badge { display: inline-block; //other container styles }
+ * .badge span:first-child{ // key styles }
+ * .badge span:last-child{ // value styles }
+ */
---
+++ tests/Unit/TileCest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Test\Unit;
+
+use Syndicate\Render\Tile;
+use Tests\Support\UnitTester;
+
+class TileCest
+{
+    public function tryTestTiles(UnitTester $I)
+    {
+        $tile = new Tile(tag: 'div');
+        $title = 'google';
+        $tile[] = $title;
+        $icon = new Tile(tag: 'div', content: '✨');
+        $tile[] = $icon;
+
+        $expected = <<<HTML
+                <div class="Tile">
+                    <button class="content">
+                        <div>✨</div>
+                        <h1>google</h1>
+                    </button>  
+                </div>
+            HTML;
+        $I->assertEquals($expected, $tile);
+    }
+}
